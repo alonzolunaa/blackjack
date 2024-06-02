dc4769d3a394c257d62949d0953c54ac068baa1f
alonzolunaa <45525625+alonzolunaa@users.noreply.github.com>
2024-06-01 21:16:20 -0600

    Add files via upload

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -53,65 +53,65 @@ public:
 
 int main() {
     while(true){
-srand(time(NULL));
-    Baraja baraja;
-    int contador = 0;
-    int numAses = 0;
-    char opc = 's';
+        srand(time(NULL));
+        Baraja baraja;
+        int contador = 0;
+        int numAses = 0;
+        char opc = 's';
 
-    while (opc == 's' || opc == 'S') {
-        Carta miCarta = baraja.sacarCarta();
+        while (opc == 's' || opc == 'S') {
+            Carta miCarta = baraja.sacarCarta();
 
-        if (miCarta.nombreCarta == "As") {
-            cout << "Ha salido un As, que valor quieres darle, 11 o 1?: ";
-            int valorAs;
-            cin >> valorAs;
-            while (valorAs != 1 && valorAs != 11) {
-                cout << "Valor incorrecto. Por favor, elige 1 o 11: ";
+            if (miCarta.nombreCarta == "As") {
+                cout << "Ha salido un As, que valor quieres darle, 11 o 1?: ";
+                int valorAs;
                 cin >> valorAs;
+                while (valorAs != 1 && valorAs != 11) {
+                    cout << "Valor incorrecto. Por favor, elige 1 o 11: ";
+                    cin >> valorAs;
+                }
+                miCarta.valorCarta = valorAs;
+                if (valorAs == 11) numAses++;
             }
-            miCarta.valorCarta = valorAs;
-            if (valorAs == 11) numAses++;
-        }
 
-        cout << "Ha salido la carta " << miCarta.nombreCarta << " de " << miCarta.tipoCarta << endl;
-        contador += miCarta.valorCarta;
+            cout << "Ha salido la carta " << miCarta.nombreCarta << " de " << miCarta.tipoCarta << endl;
+            contador += miCarta.valorCarta;
 
-        while (contador > 21 && numAses > 0) {
-            contador -= 10;
-            numAses--;
-        }
+            while (contador > 21 && numAses > 0) {
+                contador -= 10;
+                numAses--;
+            }
 
-        if (contador > 21) {
-            cout << "Tu puntaje excedio los 21 puntos, has perdido\n"<<endl;
-            cout<<"Puntaje final: "<<contador<<"\n";
-            break;
-        }
+            if (contador > 21) {
+                cout << "Tu puntaje excedio los 21 puntos, has perdido\n"<<endl;
+                cout<<"Puntaje final: "<<contador<<"\n";
+                break;
+            }
 
-        cout << "Tu puntaje actual es: " << contador << "\n\n";
-        if (contador == 21) {
-            cout << "Felicidades, has acumulado 21 puntos, ganaste\n";
-            break;
-        }
+            cout << "Tu puntaje actual es: " << contador << "\n\n";
+            if (contador == 21) {
+                cout << "Felicidades, has acumulado 21 puntos, ganaste\n";
+                break;
+            }
 
-        cout << "Quieres seguir jugando? S/N: ";
-        cin >> opc;
-        while (opc != 'n' && opc != 'N' && opc != 'S' && opc != 's') {
-            cout << "\nInput incorrecto. Por favor, ingresa S (si) o N (no): ";
+            cout << "Quieres seguir jugando? S/N: ";
             cin >> opc;
+            while (opc != 'n' && opc != 'N' && opc != 'S' && opc != 's') {
+                cout << "\nInput incorrecto. Por favor, ingresa S (si) o N (no): ";
+                cin >> opc;
+            }
+            system("cls");
         }
+
+        if (opc == 'N' || opc == 'n') {
+            Carta miCarta = baraja.sacarCarta();
+            cout << "La siguiente carta era: " << miCarta.nombreCarta << " de " << miCarta.tipoCarta << endl;
+            cout<<"Puntaje final: "<<contador<<"\n";
+        }
+
+        system("pause");
         system("cls");
-    }
-
-    if (opc == 'N' || opc == 'n') {
-        Carta miCarta = baraja.sacarCarta();
-        cout << "La siguiente carta era: " << miCarta.nombreCarta << " de " << miCarta.tipoCarta << endl;
-        cout<<"Puntaje final: "<<contador<<"\n";
-    }
-
-    system("pause");
-    system("cls");
-    return 0;
+        return 0;
     }
     
 }
